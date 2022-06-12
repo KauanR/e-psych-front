8c5d66f3771401d98ca2b8a190379f0ff500f769
KauanR <kau.matosr@gmail.com>
2022-06-12 02:15:33 -0300

    feat: home page implementation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { filter, Subscription } from 'rxjs'
+import { User } from 'src/app/core/interfaces/user'
+import { UserService } from 'src/app/core/services/user.service'
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.component.html',
     styleUrls: ['home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+
+    user: User
+    userSub: Subscription
+
+    constructor(
+        private userService: UserService
+    ) {}
+
+    ngOnInit(): void {
+        this.userSub = this.userService.user
+            .pipe(filter(user => user.id !== ''))
+            .subscribe(user => {
+                this.user = user
+            })
+    }
+
+    ngOnDestroy(): void {
+        this.userSub?.unsubscribe()
+    }
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
 
@@ -9,7 +11,10 @@ import { HomeComponent } from './home.component'
     ],
     imports: [
         CommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+
+        MatCardModule,
+        MatButtonModule
     ]
 })
 export class HomeModule {}
